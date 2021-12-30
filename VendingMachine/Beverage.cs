02b67aaccca4a2e1b40bf0be9c798cb3537d280f
Thomas Selin <selin.reso@gmail.com>
2021-12-30 11:03:59 +0100

    Add beverage class

---
+++ VendingMachine/Beverage.cs
@@ -0,0 +1,25 @@
+﻿namespace VendingMachine
+{
+    /// <summary>
+    /// Describes a beverage.
+    /// </summary>
+    public class Beverage : Product
+    {
+        /// <summary>
+        /// Initializes the beverage with a name, information and a price.
+        /// </summary>
+        /// <param name="name"><inheritdoc cref="Product.Product" path="/param[@name='name']"/></param>
+        /// <param name="information"><inheritdoc cref="Product.Product" path="/param[@name='information']"/></param>
+        /// <param name="price"><inheritdoc cref="Product.Product" path="/param[@name='price']"/></param>
+        public Beverage(string name, string information, int price) : base(name, information, price){}
+
+        /// <summary>
+        /// Returns a use message.
+        /// </summary>
+        /// <returns>The use message</returns>
+        public new string Use()
+        {
+            return $"The {Name} is being drunk.";
+        }
+    }
+}
