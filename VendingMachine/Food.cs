0950e3080a26a0f9b2ba36946c20c6a209541934
Thomas Selin <selin.reso@gmail.com>
2021-12-30 10:54:50 +0100

    Add food class

---
+++ VendingMachine/Food.cs
@@ -0,0 +1,25 @@
+﻿namespace VendingMachine
+{
+    /// <summary>
+    /// Describes a food.
+    /// </summary>
+    public class Food : Product
+    {
+        /// <summary>
+        /// Initializes the food with a name, information and a price.
+        /// </summary>
+        /// <param name="name"><inheritdoc cref="Product.Product" path="/param[@name='name']"/></param>
+        /// <param name="information"><inheritdoc cref="Product.Product" path="/param[@name='information']"/></param>
+        /// <param name="price"><inheritdoc cref="Product.Product" path="/param[@name='price']"/></param>
+        public Food(string name, string information, int price) : base(name, information, price){}
+
+        /// <summary>
+        /// Returns a use message.
+        /// </summary>
+        /// <returns>The use message</returns>
+        public new string Use()
+        {
+            return $"The {Name} is being eaten.";
+        }
+    }
+}
