225cfed9aa285b7d1b7c4d1e8dc32f8000e82b74
Thomas Selin <selin.reso@gmail.com>
2021-12-30 10:50:06 +0100

    Add candy class

---
+++ VendingMachine/Candy.cs
@@ -0,0 +1,25 @@
+﻿namespace VendingMachine
+{
+    /// <summary>
+    /// Describes a candy.
+    /// </summary>
+    public class Candy : Product
+    {
+        /// <summary>
+        /// Initializes the candy with a name, information and a price.
+        /// </summary>
+        /// <param name="name"><inheritdoc cref="Product.Product" path="/param[@name='name']"/></param>
+        /// <param name="information"><inheritdoc cref="Product.Product" path="/param[@name='information']"/></param>
+        /// <param name="price"><inheritdoc cref="Product.Product" path="/param[@name='price']"/></param>
+        public Candy(string name, string information, int price) : base(name, information, price){}
+
+        /// <summary>
+        /// Returns a use message.
+        /// </summary>
+        /// <returns>The use message</returns>
+        public new string Use()
+        {
+            return $"The {Name} is being eaten.";
+        }
+    }
+}
