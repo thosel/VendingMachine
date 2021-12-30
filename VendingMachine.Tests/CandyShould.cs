f0fb16ed9f43f80df87d2aea1a35a2f36ba35ba9
Thomas Selin <selin.reso@gmail.com>
2021-12-30 10:49:45 +0100

    Add test class for candy

---
+++ VendingMachine.Tests/CandyShould.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace VendingMachine.Tests
+{
+    /// <summary>
+    /// The test class for the candy class.
+    /// </summary>
+    public class CandyShould
+    {
+        private Candy _sut;
+
+        /// <summary>
+        /// Initializes a candy instance to be the system under test.
+        /// </summary>
+        public CandyShould()
+        {
+            _sut = new Candy("Candy", "Information", 10);
+        }
+
+        /// <summary>
+        /// Asserts that the candy returns the correct use message.
+        /// </summary>
+        [Fact]
+        public void ReturnCorrectUseMessage()
+        {
+            string useMessage = _sut.Use();
+            Assert.Equal("The Candy is being eaten.", useMessage);
+        }
+    }
+}
